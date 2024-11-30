94d64712dc85bda4d9dedeed103534109ca2d608
Sholastix <drenai6436@gmail.com>
2024-11-30 15:33:40 +0200

    SERVER: 1. Basic server ready. 2. ".env" file added.

--- server/main.js
+++ server/main.js
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const server = require('node:http').createServer(app);
+
+// Imports.
+const dbConnection = require('./src/config/dbConnection');
+
+// Environment variables.
+const PORT = process.env.PORT;
+
+// Initialize the middleware.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: ['http://127.0.0.1:5173']
+}));
+
+// Starting the server.
+server.listen(PORT, () => {
+  console.log(`\nServer running at 'http://localhost:${PORT}'.`);
+});
+
+// Database connection.
+dbConnection();
---
+++ server/src/config/dbConnection.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log('\nDatabase connected.');
+  } catch (err) {
+    console.error(`Connection failed: ${MONGODB_URI}`, err.message);
+    process.exit(1);
+  };
+};
+
+module.exports = dbConnection;
