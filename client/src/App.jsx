602c1ea12d3020d3ee479faf497011f680bdec60
Sholastix <drenai@gmail.com>
2024-12-21 17:03:57 +0200

    Added and implemented Component 'ProtectedRoutes' which works as wrapper for private routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import styles from './App.module.css';
 
-import ErrorPage from './components/ErrorPage/ErrorPage';
 import Chat from './features/chat/Chat';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes';
 import Signin from './features/signin/Signin';
 import Signup from './features/signup/Signup';
 
@@ -14,7 +15,9 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Navigate replace to='/signup' />} />
-          <Route path='/chat' element={<Chat />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path='/chat' element={<Chat />} />
+          </Route>
           <Route path='/signin' element={<Signin />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='*' element={<ErrorPage />} />
--- client/src/features/chat/Chat.jsx
+++ client/src/features/chat/Chat.jsx
@@ -1,30 +1,9 @@
-import { useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
-// 'useSelector' hook used to get hold of any STATE that is maintained in the Redux STORE.
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
 
 // Styles.
 import styles from './Chat.module.css';
 
-import { isUserSignedIn } from '../auth/authSlice';
-
 const Chat = () => {
-  // This hook accepts a selector function as its parameter. Function receives Redux STATE as argument.
-  const authState = useSelector((state) => {
-    return state.authReducer
-  });
-
-  // This constant will be used to dispatch ACTIONS when we need it.
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    dispatch(isUserSignedIn());
-  }, []);
-
-  if (!authState.loading && !authState.isAuthenticated) {
-    return <Navigate to='/signin' replace={true} />;
-  };
-
   return (
     <div className={styles.container}>Chat</div>
   );
