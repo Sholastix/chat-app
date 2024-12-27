f22ff36736f2283ee072a4c5d96a5aff1bb16363
Sholastix <drenai@gmail.com>
2024-12-27 21:05:13 +0200

    CLIENT: auth check added to 'App.jsx'. If user already authenticated and authorized then he will be automatically redirected to 'Chat' page.

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,5 +1,6 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import styles from './App.module.css';
 
@@ -9,7 +10,17 @@ import ProtectedRoutes from '../components/ProtectedRoutes/ProtectedRoutes';
 import Signin from '../features/auth/signin/Signin';
 import Signup from '../features/auth/signup/Signup';
 
+import { isUserSignedIn } from '../features/auth/authSlice';
+
 const App = () => {
+  // This constant will be used to dispatch ACTIONS when we need it.
+  const dispatch = useDispatch();
+
+  // Every time the page reloads, we check if the user is logged in.
+  useEffect(() => {
+    dispatch(isUserSignedIn());
+  }, []);
+
   return (
     <Fragment>
       <BrowserRouter>
