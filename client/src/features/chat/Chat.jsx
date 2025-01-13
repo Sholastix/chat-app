1020d12b9532c8ff2315cfcd5425a4d4fac1530a
Sholastix <drenai@gmail.com>
2025-01-13 17:53:58 +0200

    CLIENT: 1. 'Spinner' Component integrated in 'Header' Component. 2. Removed unnecessary conditions from 'Chat' component.

--- client/src/features/chat/Chat.jsx
+++ client/src/features/chat/Chat.jsx
@@ -1,3 +1,6 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
 // Styles.
 import styles from './Chat.module.css';
 
@@ -6,20 +9,34 @@ import ChatBox from '../../components/ChatBox/ChatBox';
 import ChatsList from '../../components/ChatsList/ChatsList';
 import Header from '../../components/Header/Header';
 import LeftDrawer from '../../components/ModalWindows/LeftDrawer/LeftDrawer';
+import Spinner from '../../components/Spinner/Spinner';
 
 // Functions.
 import { socket } from '../../socket/socket';
 
 const Chat = () => {
+  // This hook accepts a selector function as its parameter. Function receives Redux STATE as argument.
+  const authState = useSelector((state) => {
+    return state.authReducer
+  });
+
   return (
-    <div className={styles.container}>
-      <Header />
-      <div className={styles.chatContainer}>
-        <LeftDrawer />
-        <ChatBox />
-        <ChatsList />
-      </div>
-    </div>
+    <Fragment>
+      {
+        !authState.loading && authState.user
+          ?
+          <div className={styles.container}>
+            <Header />
+            <div className={styles.chatContainer}>
+              <LeftDrawer />
+              <ChatBox />
+              <ChatsList />
+            </div>
+          </div>
+          :
+          <Spinner />
+      }
+    </Fragment>
   );
 };
 
