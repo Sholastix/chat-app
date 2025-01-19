dee950a68741cf772d3281b8aff05835c002be9d
Sholastix <drenai@gmail.com>
2025-01-19 17:54:21 +0200

    CLIENT: UI improved.

--- client/src/components/UserListItem/UserListItem.jsx
+++ client/src/components/UserListItem/UserListItem.jsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Avatar, Box, Typography } from '@mui/material';
 
 const UserListItem = (props) => {
   return (
-    <Typography
-      component='div'
+    <Box
       sx={{
-        scrollBehavior: 'smooth',
+        alignItems: 'center',
         border: '0.1rem solid lightgray',
         borderRadius: '0.5rem',
         cursor: 'pointer',
+        display: 'flex',
         fontSize: '1.4rem',
         marginTop: '1rem',
-        padding: '1rem 0rem',
+        padding: '1rem 1.5rem',
         ':hover': { boxShadow: '0 0.2rem 1rem 0 rgba(0, 0, 0, 0.3)' }
       }}
       onClick={props.handleFunction}
     >
-      {props.user.username}
-    </Typography>
+      <Avatar
+        src={props.user.avatar}
+        sx={{ marginRight: '1rem' }}
+      />
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start'
+        }}
+      >
+        <Typography
+          component='div'
+          sx={{ fontSize: '1.4rem' }}
+        >
+          {props.user.username}
+        </Typography>
+
+        <Typography
+          component='div'
+          sx={{ fontSize: '1.2rem' }}
+        >
+          <strong>Email:</strong> {props.user.email}
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
