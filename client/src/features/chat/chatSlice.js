db75d386ce7f670962deb4763bd3dd8875723af2
Sholastix <drenai@gmail.com>
2025-01-21 18:00:57 +0200

    CLIENT: 'createPrivateChat' function modified.

--- client/src/features/chat/chatSlice.js
+++ client/src/features/chat/chatSlice.js
@@ -9,21 +9,46 @@ const initialState = {
   selectedChat: null
 };
 
+// Create new 1-on-1 chat or return existed chat.
 export const createPrivateChat = createAsyncThunk('chat/createPrivateChat', async (userId) => {
   try {
-    // Return newly created 1-on-1 chat.
-    const { data } = await axios.post('http://localhost:5000/api/chat', { userId });
+    // // VARIANT 1.
+    // const { data } = await axios.post('http://localhost:5000/api/chat', { userId });
+    // return data;
+    
+    // VARIANT 2.
+    let allChats = await axios.get('http://localhost:5000/api/chat');
+    console.log('FETCH_CHATS: ', allChats.data);
 
-    // // If that chat already in our chats list then we just update the chats list. 
-    // if (chats.find((chat) => chat._id === data._id)) {
-    //   setChats([data, ...chats]);
-    // };
+    const createdChat = await axios.post('http://localhost:5000/api/chat', { userId });
+    console.log('CREATED_CHAT: ', createdChat.data);
 
-    return data;
+    const isChatExists = allChats.data.find((chat) => chat._id === createdChat.data._id);
+    console.log('CHECK: ', isChatExists);
+
+    // If that chat already existed in database then we don't add it to our chats list again. 
+    if (isChatExists === undefined) {
+      allChats.data.push(createdChat.data);
+    };
+
+    console.log('FETCH_CHATS_UPDATED: ', allChats.data);
+    return { createdChat: createdChat.data, allChats: allChats.data };
   } catch (err) {
     console.error(err);
   };
 });
+
+// // Get all current user's chats.
+// export const fetchChats = createAsyncThunk('chat/fetchChats', async () => {
+//   try {
+//     const { data } = await axios.get('http://localhost:5000/api/chat');
+//     console.log('FETCH_CHATS: ', data);
+
+//     return data;
+//   } catch (err) {
+//     console.error(err);
+//   };
+// });
 
 // Create slice of the STORE for 'Chat'.
 const chatSlice = createSlice({
@@ -37,16 +62,25 @@ const chatSlice = createSlice({
   extraReducers: (builder) => {
     // -------------------------------   CREATE 1-on-1 CHAT   -------------------------------
 
-    // Commented because we set initial STATE value of 'loading' to 'true'.
     builder.addCase(createPrivateChat.pending, (state, action) => {
       state.loading = true
     });
 
+    // // VARIANT 1.
+    // builder.addCase(createPrivateChat.fulfilled, (state, action) => {
+    //   console.log('PAYLOAD: ', action.payload);
+    //   state.loading = false,
+    //   state.error = '',
+    //   state.chats = [...state.chats, action.payload],
+    //   state.selectedChat = action.payload
+    // });
+
+    // VARIANT 2.
     builder.addCase(createPrivateChat.fulfilled, (state, action) => {
       state.loading = false,
       state.error = '',
-      state.chats = [],
-      state.selectedChat = action.payload
+      state.chats = action.payload.allChats,
+      state.selectedChat = action.payload.createdChat
     });
 
     builder.addCase(createPrivateChat.rejected, (state, action) => {
@@ -55,6 +89,26 @@ const chatSlice = createSlice({
       state.chats = [],
       state.selectedChat = null
     });
+
+    // -------------------------------   GET ALL CURRENT USER'S CHATS   -------------------------------
+
+    // builder.addCase(fetchChats.pending, (state, action) => {
+    //   state.loading = true
+    // });
+
+    // builder.addCase(fetchChats.fulfilled, (state, action) => {
+    //   state.loading = false,
+    //   state.error = '',
+    //   state.chats = action.payload,
+    //   state.selectedChat = null
+    // });
+
+    // builder.addCase(fetchChats.rejected, (state, action) => {
+    //   state.loading = false,
+    //   state.error = action.error.message,
+    //   state.chats = [],
+    //   state.selectedChat = null
+    // });
   },
 });
 
