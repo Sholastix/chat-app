5b1f5b0a9358fb38fb521ae3c6d355feb7b405cb
Sholastix <drenai6436@gmail.com>
2025-01-23 14:54:00 +0200

    SERVER: npm dependencies updated. CLIENT: rendering condition added in 'ChatsList' Component.

--- client/src/components/ChatsList/ChatsList.jsx
+++ client/src/components/ChatsList/ChatsList.jsx
@@ -111,44 +111,49 @@ const ChatsList = () => {
           width: '100%'
         }}
       >
-        {/* {chatState.loading && <Spinner />} */}
-        <Stack
-          sx={{
-            overflowY: 'auto',
-            width: '100%'
-          }}
-        >
-          {
-            chatState.chats.map((chat) => (
-              <Box
-                component='div'
-                key={chat._id}
-                sx={{
-                  backgroundColor: 'white',
-                  border: '0.1rem solid lightgray',
-                  borderRadius: '0.5rem',
-                  color: 'black',
-                  cursor: 'pointer',
-                  marginBottom: '1rem',
-                  padding: '2rem 3rem',
-                  // width: '100%'
-                  ':hover': { boxShadow: '0 0.2rem 1rem 0 rgba(0, 0, 0, 0.3)' },
-                }}
-                // Later we must add here an action which will set 'selectedChat' property from chat STATE to current mapped chat (ex.: 'onClick={() => setSelectedChat(chat)}').
-                onClick={() => console.log('Hello, World')}
-              >
-                <Typography
-                  sx={{
-                    fontSize: '1.4rem',
-                    fontWeight: '500'
-                  }}
-                >
-                  {!chat.isGroupChat ? getSender(authState.user, chat.users) : (chat.chatName)}
-                </Typography>
-              </Box>
-            ))
-          }
-        </Stack>
+        {
+          chatState.chats
+            ?
+            <Stack
+              sx={{
+                overflowY: 'auto',
+                width: '100%'
+              }}
+            >
+              {
+                chatState.chats.map((chat) => (
+                  <Box
+                    component='div'
+                    key={chat._id}
+                    sx={{
+                      backgroundColor: 'white',
+                      border: '0.1rem solid lightgray',
+                      borderRadius: '0.5rem',
+                      color: 'black',
+                      cursor: 'pointer',
+                      marginBottom: '1rem',
+                      padding: '2rem 3rem',
+                      // width: '100%'
+                      ':hover': { boxShadow: '0 0.2rem 1rem 0 rgba(0, 0, 0, 0.3)' },
+                    }}
+                    // Later we must add here an action which will set 'selectedChat' property from chat STATE to current mapped chat (ex.: 'onClick={() => setSelectedChat(chat)}').
+                    onClick={() => console.log('Hello, World')}
+                  >
+                    <Typography
+                      sx={{
+                        fontSize: '1.4rem',
+                        fontWeight: '500'
+                      }}
+                    >
+                      {!chat.isGroupChat ? getSender(authState.user, chat.users) : (chat.chatName)}
+                    </Typography>
+                  </Box>
+                ))
+              }
+            </Stack>
+            :
+            <Spinner />
+        }
       </Box>
     </Box>
   );
