f5b766a1f2fc10ce929aaa6bb1038dcd14893098
Sholastix <drenai6436@gmail.com>
2025-02-13 16:08:08 +0200

    CLIENT: added close icon to 'UserBadgeItem' Component.

--- client/src/components/UserBadgeItem/UserBadgeItem.jsx
+++ client/src/components/UserBadgeItem/UserBadgeItem.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
 
+// MUI Icons.
+import CloseIcon from '@mui/icons-material/Close';
+
 const UserBadgeItem = (props) => {
   return (
     <Box
@@ -8,22 +11,22 @@ const UserBadgeItem = (props) => {
         alignItems: 'center',
         backgroundColor: 'rgb(93, 109, 126)',
         borderRadius: '0.5rem',
+        color: 'white',
         cursor: 'pointer',
+        display: 'flex',
         margin: '0rem 0.5rem 1rem',
-        padding: '0.5rem 1rem',
+        padding: '0.5rem',
         ':hover': { boxShadow: '0 0.2rem 1rem 0 rgba(0, 0, 0, 0.3)' }
       }}
       onClick={props.handleFunction}
     >
       <Typography
         component='div'
-        sx={{
-          color: 'white',
-          fontSize: '1.2rem',
-        }}
+        sx={{ fontSize: '1.2rem' }}
       >
         {props.user.username}
       </Typography>
+      <CloseIcon sx={{ marginLeft: '0.3rem', fontSize: '1.4rem' }} />
     </Box>
   );
 };
