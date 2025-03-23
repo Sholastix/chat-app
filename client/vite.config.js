2a9b3ad9d3545c2fbe7c8ec7a06cc1906f1d48f2
Sholastix <drenai6436@gmail.com>
2025-03-23 11:05:03 +0200

    CLIENT: minor changes to proxy setup.

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    // BENEFITS OF PROXY:
+    // - avoid CORS issues (the proxy makes it seem like your requests are coming from the same origin as the backend server).
+    // - simplified API calls (no need to hardcode full URLs in your frontend code. Simply use relative paths like '/api/users').
+    // - flexible development setup (switch backend servers easily by updating the 'target' value in your config).
     proxy: {
-      '/api': 'http://localhost:5000'
+      // Proxy Key: the '/api' key tells Vite to intercept any requests starting with /api.
+      '/api': {
+        // Target Option: requests matching /api are forwarded to Backend server address http://localhost:5000.
+        target: 'http://localhost:5000',
+        // Ensures that the proxy adjusts the Host header to match the target server (basically, ensures the request is coming from frontend server).
+        changeOrigin: true,
+        // // Modifies the request path before forwarding. For example, '/api/users' becomes '/users'.
+        // rewrite: (path) => path.replace(/^\/api/, ''), // Optional: remove '/api' prefix.
+      },
     }
   }
 });
