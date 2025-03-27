0df87e14451b884c1f1c93ca4ac1307c29279dcf
Sholastix <drenai6436@gmail.com>
2025-03-27 17:43:06 +0200

    CLIENT: additional login check has been restored.

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,8 +1,10 @@
-import { Fragment } from 'react';
+import { useEffect, Fragment } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import './App.css';
 
+// Components.
 import Chat from '../features/chat/Chat';
 import ErrorPage from '../components/ErrorPage/ErrorPage';
 import ProtectedRoutes from '../components/ProtectedRoutes/ProtectedRoutes';
@@ -10,7 +12,17 @@ import Signin from '../features/auth/signin/Signin';
 import Signup from '../features/auth/signup/Signup';
 import UserProfilePage from '../components/UserProfilePage/UserProfilePage';
 
+// Functions.
+import { isUserSignedIn } from '../features/auth/authSlice';
+
 const App = () => {
+  // This constant will be used to dispatch ACTIONS when we need it.
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(isUserSignedIn());
+  }, []);
+
   return (
     <Fragment>
       <BrowserRouter>
