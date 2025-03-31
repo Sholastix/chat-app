5b48d8679c909585a153eb84eef75448b4639cb4
Sholastix <drenai6436@gmail.com>
2025-03-31 20:00:00 +0300

    CLIENT: socket URL fixed.

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_HOST;
+const URL = 'http://localhost:5000';
 console.log('URL: ', URL);
 
 export const socket = io(URL, { autoConnect: true });
