10950c6cadd8acba3a8c0a2c04eeae154d6e014e
Sholastix <drenai6436@gmail.com>
2025-03-31 21:30:26 +0300

    CLIENT: minor changes to socket URL.

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const URL = '/';
+const URL = import.meta.env.MODE === 'development' ? 'http://localhost:5000' : '/';
+console.log('IMPORT_MODE: ', import.meta.env.MODE);
 console.log('URL: ', URL);
 
 export const socket = io(URL, { autoConnect: true });
