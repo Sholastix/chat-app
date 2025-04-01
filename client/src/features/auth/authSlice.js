ea411ae7badccfac1751c474451feac2324656aa
Sholastix <drenai6436@gmail.com>
2025-04-01 21:12:54 +0300

    CLIENT: big and VERY IMPORTANT cleaning.

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -20,70 +20,54 @@ const initialState = {
 // We can listen to this ACTION types with an EXTRA_REDUCER and performes the necessary STATE transitions.
 // Auth check.
 export const isUserSignedIn = createAsyncThunk('auth/isUserSignedIn', async () => {
-  try {
-    if (localStorage.token) {
-      setAuthToken();
-    };
-
-    const user = await axios.get('/api/auth');
-
-    return user.data;
-  } catch (err) {
-    console.error(err);
+  if (localStorage.token) {
+    setAuthToken();
   };
+
+  const user = await axios.get('/api/auth');
+
+  return user.data;
 });
 
 // Signup.
 export const signup = createAsyncThunk('auth/signup', async (props) => {
-  try {
-    const user = await axios.post('/api/signup', {
-      username: props.username,
-      email: props.email,
-      password: props.password,
-      confirmPassword: props.confirmPassword
-    });
+  const user = await axios.post('/api/signup', {
+    username: props.username,
+    email: props.email,
+    password: props.password,
+    confirmPassword: props.confirmPassword
+  });
 
-    return user.data;
-  } catch (err) {
-    console.error(err);
-  };
+  return user.data;
 });
 
 // Signin.
 export const signin = createAsyncThunk('auth/signin', async (props) => {
-  try {
-    const user = await axios.post('/api/signin', {
-      email: props.email,
-      password: props.password
-    });
+  const user = await axios.post('/api/signin', {
+    email: props.email,
+    password: props.password
+  });
 
-    return user.data;
-  } catch (err) {
-    console.error(err);
-  };
+  return user.data;
 });
 
 // Update user's profile.
 export const updateUser = createAsyncThunk('auth/updateUser', async ({ id, picture, username, updNotifications }) => {
-  try {
-    // If we updating picture and name from 'user profile' form.
-    if (picture && username) {
-      const { data } = await axios.put(`/api/user/${id}`, {
-        picture: picture,
-        username: username
-      });
-  
-      return data;
-    } else {
-      // If we updating only notifications.
-      const { data } = await axios.put(`/api/user/${id}`, {
-        updNotifications: updNotifications
-      });
-  
-      return data;
-    };
-  } catch (err) {
-    console.error(err);
+  // If we updating picture and name from 'user profile' form.
+  if (picture && username) {
+    const { data } = await axios.put(`/api/user/${id}`, {
+      picture: picture,
+      username: username
+    });
+
+    return data;
+  } else {
+    // If we updating only notifications.
+    const { data } = await axios.put(`/api/user/${id}`, {
+      updNotifications: updNotifications
+    });
+
+    return data;
   };
 });
 
@@ -99,12 +83,12 @@ const authSlice = createSlice({
     signout: (state, action) => {
       // Mutating the STATE directly is possible due to 'redux-toolkit' using npm 'Immer' under the hood.
       state.loading = false;
-      state.error = '',
-      state.user = null,
-      state.isAuthenticated = false,
-      state.token = null
+      state.error = '';
+      state.user = null;
+      state.isAuthenticated = false;
+      state.token = null;
       // Remove token from local storage.
-      localStorage.removeItem('token')
+      localStorage.removeItem('token');
       // Disconnect from socket server.
       socket.disconnect();
     }
@@ -115,26 +99,26 @@ const authSlice = createSlice({
 
     // Commented because we set initial STATE value of 'loading' to 'true'.
     builder.addCase(isUserSignedIn.pending, (state, action) => {
-      state.loading = true
+      state.loading = true;
     });
 
     builder.addCase(isUserSignedIn.fulfilled, (state, action) => {
-      state.loading = false,
-      state.error = '',
-      state.user = action.payload,
-      state.isAuthenticated = true
-      // // Connect to socket server.
-      // socket.connect();
+      state.loading = false;
+      state.error = '';
+      state.user = action.payload;
+      state.isAuthenticated = true;
+      // Connect to socket server.
+      socket.connect();
     });
 
     builder.addCase(isUserSignedIn.rejected, (state, action) => {
       state.loading = false;
-      state.error = action.error.message,
-      state.user = null,
-      state.isAuthenticated = false,
-      state.token = null,
+      state.error = action.error.message;
+      state.user = null;
+      state.isAuthenticated = false;
+      state.token = null;
       // Remove token from local storage.
-      localStorage.removeItem('token')
+      localStorage.removeItem('token');
       // Disconnect from socket server.
       socket.disconnect();
     });
@@ -142,53 +126,53 @@ const authSlice = createSlice({
     // -------------------------------   SIGNUP   -------------------------------
 
     builder.addCase(signup.pending, (state, action) => {
-      state.loading = true
+      state.loading = true;
     });
 
     builder.addCase(signup.fulfilled, (state, action) => {
-      state.loading = false,
-      state.error = '',
-      state.user = action.payload,
-      state.isAuthenticated = true,
-      state.token = action.payload.token,
+      state.loading = false;
+      state.error = '';
+      state.user = action.payload;
+      state.isAuthenticated = true;
+      state.token = action.payload.token;
       // Put token in local storage.
-      localStorage.setItem('token', action.payload.token)
+      localStorage.setItem('token', action.payload.token);
     });
 
     builder.addCase(signup.rejected, (state, action) => {
       state.loading = false;
-      state.error = action.error.message,
-      state.user = null,
-      state.isAuthenticated = false,
-      state.token = null,
+      state.error = action.error.message;
+      state.user = null;
+      state.isAuthenticated = false;
+      state.token = null;
       // Remove token from local storage.
-      localStorage.removeItem('token')
+      localStorage.removeItem('token');
     });
 
     // -------------------------------   SIGNIN   -------------------------------
 
     builder.addCase(signin.pending, (state, action) => {
-      state.loading = true
+      state.loading = true;
     });
 
     builder.addCase(signin.fulfilled, (state, action) => {
-      state.loading = false,
-      state.error = '',
-      state.user = action.payload,
-      state.isAuthenticated = true,
-      state.token = action.payload.token,
+      state.loading = false;
+      state.error = '';
+      state.user = action.payload;
+      state.isAuthenticated = true;
+      state.token = action.payload.token;
       // Put token in local storage.
-      localStorage.setItem('token', action.payload.token)
+      localStorage.setItem('token', action.payload.token);
     });
 
     builder.addCase(signin.rejected, (state, action) => {
-      state.loading = false,
-      state.error = action.error.message,
-      state.user = null,
-      state.isAuthenticated = false,
-      state.token = null
+      state.loading = false;
+      state.error = action.error.message;
+      state.user = null;
+      state.isAuthenticated = false;
+      state.token = null;
       // Remove token from local storage.
-      localStorage.removeItem('token')
+      localStorage.removeItem('token');
     });
 
     // -------------------------------   UPDATE USER'S PROFILE   -------------------------------
--- client/src/features/chat/chatSlice.js
+++ client/src/features/chat/chatSlice.js
@@ -12,102 +12,74 @@ const initialState = {
 
 // Get all chats of the current user.
 export const fetchChats = createAsyncThunk('chat/fetchChats', async () => {
-  try {
-    const { data } = await axios.get('/api/chat');
+  const { data } = await axios.get('/api/chat');
 
-    return data;
-  } catch (err) {
-    console.error(err);
-  };
+  return data;
 });
 
 // Get one specific chat of the current user.
 export const fetchChat = createAsyncThunk('chat/fetchChat', async (chatId) => {
-  try {
-    const { data } = await axios.get(`/api/chat/${chatId}`);
+  const { data } = await axios.get(`/api/chat/${chatId}`);
 
-    return data;
-  } catch (err) {
-    console.error(err);
-  };
+  return data;
 });
 
 // Create new private chat or return it if it exists already.
 export const createPrivateChat = createAsyncThunk('chat/createPrivateChat', async (userId) => {
-  try {
-    let allChats = await axios.get('/api/chat');
-    const createdChat = await axios.post('/api/chat', { userId });
-    const isChatExists = allChats.data.find((chat) => chat._id === createdChat.data._id);
+  let allChats = await axios.get('/api/chat');
+  const createdChat = await axios.post('/api/chat', { userId });
+  const isChatExists = allChats.data.find((chat) => chat._id === createdChat.data._id);
 
-    // If that chat already existed in database then we don't add it to our chats list again. 
-    if (isChatExists === undefined) {
-      allChats.data.push(createdChat.data);
-    };
+  // If that chat already existed in database then we don't add it to our chats list again. 
+  if (isChatExists === undefined) {
+    allChats.data.push(createdChat.data);
+  };
 
-    return {
-      createdChat: createdChat.data,
-      allChats: allChats.data
-    };
-  } catch (err) {
-    console.error(err);
+  return {
+    createdChat: createdChat.data,
+    allChats: allChats.data
   };
 });
 
 // Create group chat.
 export const createGroupChat = createAsyncThunk('chat/createGroupChat', async ({ chatName, users }) => {
-  try {
-    let allChats = await axios.get('/api/chat');
-    const createdGroupChat = await axios.post('/api/chat/group', { chatName, users });
-    const isChatExists = allChats.data.find((chat) => chat._id === createdGroupChat.data._id);
+  let allChats = await axios.get('/api/chat');
+  const createdGroupChat = await axios.post('/api/chat/group', { chatName, users });
+  const isChatExists = allChats.data.find((chat) => chat._id === createdGroupChat.data._id);
 
-    // If that chat already existed in database then we don't add it to our chats list again. 
-    if (isChatExists === undefined) {
-      allChats.data.push(createdGroupChat.data);
-    };
+  // If that chat already existed in database then we don't add it to our chats list again. 
+  if (isChatExists === undefined) {
+    allChats.data.push(createdGroupChat.data);
+  };
 
-    return {
-      createdGroupChat: createdGroupChat.data,
-      allChats: allChats.data
-    };
-  } catch (err) {
-    console.error(err);
+  return {
+    createdGroupChat: createdGroupChat.data,
+    allChats: allChats.data
   };
 });
 
 // Rename group chat.
 export const renameGroupChat = createAsyncThunk('chat/renameGroupChat', async ({ chatId, chatName }) => {
-  try {
-    const { data } = await axios.put('/api/chat/group/rename', { chatId, chatName });
+  const { data } = await axios.put('/api/chat/group/rename', { chatId, chatName });
 
-    return data;
-  } catch (err) {
-    console.error(err);
-  };
+  return data;
 });
 
 // Add user to group chat.
 export const addUserToGroupChat = createAsyncThunk('chat/addUserToGroupChat', async ({ chatId, userId }) => {
-  try {
-    const { data } = await axios.put('/api/chat/group/add', { chatId, userId });
+  const { data } = await axios.put('/api/chat/group/add', { chatId, userId });
 
-    return data;
-  } catch (err) {
-    console.error(err);
-  };
+  return data;
 });
 
 // Remove user from group chat.
 export const removeUserFromGroupChat = createAsyncThunk('chat/removeUserFromGroupChat', async ({ chatId, userId, currentUserId }) => {
-  try {
-    const { data } = await axios.put('/api/chat/group/remove', { chatId, userId });
+  const { data } = await axios.put('/api/chat/group/remove', { chatId, userId });
 
-    if (userId === currentUserId) {
-      return null;
-    } else {
-      return data;
-    };
-  } catch (err) {
-    console.error(err);
+  if (userId === currentUserId) {
+    return null;
+  } else {
+    return data;
   };
 });
 
@@ -136,127 +108,127 @@ const chatSlice = createSlice({
     // -------------------------------   FETCH CHATS   -------------------------------
 
     builder.addCase(fetchChats.pending, (state, action) => {
-      state.loading = true
+      state.loading = true;
     });
 
     builder.addCase(fetchChats.fulfilled, (state, action) => {
-      state.loading = false,
-      state.error = '',
-      state.chats = action.payload
+      state.loading = false;
+      state.error = '';
+      state.chats = action.payload;
     });
 
     builder.addCase(fetchChats.rejected, (state, action) => {
-      state.loading = false,
-      state.error = action.error.message,
-      state.chats = [],
-      state.selectedChat = null
+      state.loading = false;
+      state.error = action.error.message;
+      state.chats = [];
+      state.selectedChat = null;
     });
 
     // -------------------------------   FETCH CHAT   -------------------------------
 
     builder.addCase(fetchChat.pending, (state, action) => {
-      state.loading = true
+      state.loading = true;
     });
 
     builder.addCase(fetchChat.fulfilled, (state, action) => {
-      state.loading = false,
-      state.error = '',
-      state.selectedChat = action.payload
+      state.loading = false;
+      state.error = '';
+      state.selectedChat = action.payload;
     });
 
     builder.addCase(fetchChat.rejected, (state, action) => {
-      state.loading = false,
-      state.error = action.error.message,
-      state.selectedChat = null
+      state.loading = false;
+      state.error = action.error.message;
+      state.selectedChat = null;
     });
 
     // -------------------------------   CREATE PRIVATE CHAT   -------------------------------
 
     builder.addCase(createPrivateChat.pending, (state, action) => {
-      state.loading = true
+      state.loading = true;
     });
 
     builder.addCase(createPrivateChat.fulfilled, (state, action) => {
-      state.loading = false,
-      state.error = '',
-      state.chats = action.payload?.allChats,
-      state.selectedChat = action.payload?.createdChat
+      state.loading = false;
+      state.error = '';
+      state.chats = action.payload?.allChats;
+      state.selectedChat = action.payload?.createdChat;
     });
 
     builder.addCase(createPrivateChat.rejected, (state, action) => {
-      state.loading = false,
-      state.error = action.error.message,
-      state.selectedChat = null
+      state.loading = false;
+      state.error = action.error.message;
+      state.selectedChat = null;
     });
 
     // -------------------------------   CREATE GROUP CHAT   -------------------------------
 
     builder.addCase(createGroupChat.pending, (state, action) => {
-      state.loading = true
+      state.loading = true;
     });
 
     builder.addCase(createGroupChat.fulfilled, (state, action) => {
-      state.loading = false,
-      state.error = '',
-      state.chats = action.payload?.allChats,
-      state.selectedChat = action.payload?.createdGroupChat
+      state.loading = false;
+      state.error = '';
+      state.chats = action.payload?.allChats;
+      state.selectedChat = action.payload?.createdGroupChat;
     });
 
     builder.addCase(createGroupChat.rejected, (state, action) => {
-      state.loading = false,
-      state.error = action.error.message,
-      state.selectedChat = null
+      state.loading = false;
+      state.error = action.error.message;
+      state.selectedChat = null;
     });
 
     // -------------------------------   RENAME GROUP CHAT   -------------------------------
 
     builder.addCase(renameGroupChat.pending, (state, action) => {
-      state.loading = true
+      state.loading = true;
     });
 
     builder.addCase(renameGroupChat.fulfilled, (state, action) => {
-      state.loading = false,
-      state.error = '',
-      state.selectedChat = action.payload
+      state.loading = false;
+      state.error = '';
+      state.selectedChat = action.payload;
     });
 
     builder.addCase(renameGroupChat.rejected, (state, action) => {
-      state.loading = false,
-      state.error = action.error.message
+      state.loading = false;
+      state.error = action.error.message;
     });
 
     // -------------------------------   ADD USER TO GROUP CHAT   -------------------------------
 
     builder.addCase(addUserToGroupChat.pending, (state, action) => {
-      state.loading = true
+      state.loading = true;
     });
 
     builder.addCase(addUserToGroupChat.fulfilled, (state, action) => {
-      state.loading = false,
-      state.error = '',
-      state.selectedChat = action.payload
+      state.loading = false;
+      state.error = '';
+      state.selectedChat = action.payload;
     });
 
     builder.addCase(addUserToGroupChat.rejected, (state, action) => {
-      state.loading = false,
-      state.error = action.error.message
+      state.loading = false;
+      state.error = action.error.message;
     });
 
     // -------------------------------   REMOVE USER FROM GROUP CHAT   -------------------------------
 
     builder.addCase(removeUserFromGroupChat.pending, (state, action) => {
-      state.loading = true
+      state.loading = true;
     });
 
     builder.addCase(removeUserFromGroupChat.fulfilled, (state, action) => {
-      state.loading = false,
-      state.error = '',
-      state.selectedChat = action.payload
+      state.loading = false;
+      state.error = '';
+      state.selectedChat = action.payload;
     });
 
     builder.addCase(removeUserFromGroupChat.rejected, (state, action) => {
-      state.loading = false,
-      state.error = action.error.message
+      state.loading = false;
+      state.error = action.error.message;
     });
   },
 });
