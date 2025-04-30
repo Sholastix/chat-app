0c7f247a9f22007e630459a5b9451edc9778c312
Sholastix <drenai6436@gmail.com>
2025-04-30 17:55:32 +0300

    CLIENT: refactored function.

--- client/src/helpers/setAuthToken.js
+++ client/src/helpers/setAuthToken.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 // If there is token in LocalStorage, then we put it in global header.
 const setAuthToken = () => {
+  const token = localStorage.getItem('token');
+
   // 'Authorization' - that's how we named this variable on server side ('middleware' dir -> 'authMdw.js').
-  if (localStorage.token) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.token}`;
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   } else {
     delete axios.defaults.headers.common['Authorization'];
   };
