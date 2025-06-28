bacb5afea32531418009c8a979b94cf4271ca2c0
Sholastix <drenai6436@gmail.com>
2025-06-28 19:11:05 +0300

    CLIENT: 1. Added lazy loading to imported Components. 2. Improved the way of getting 'selectedChat' value from chat STATE (now it will not cause unnecessary re-renderings). 3. Added 'displayName' property for Component (for debugging purposes).

--- client/src/components/ChatBox/ChatBox.jsx
+++ client/src/components/ChatBox/ChatBox.jsx
@@ -1,11 +1,16 @@
+import { lazy, Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { Box } from '@mui/material';
 
 // Components.
-import SingleChat from '../SingleChat/SingleChat';
+import Spinner from '../Spinner/Spinner';
 
-const ChatBox = (props) => {
-  const chatState = useSelector((state) => {
+// Components (lazy-loaded).
+const SingleChat = lazy(() => import('../SingleChat/SingleChat'));
+
+const ChatBox = ({ fetchAgain, setFetchAgain }) => {
+  // Get 'selectedChat' from chat STATE.
+  const { selectedChat } = useSelector((state) => {
     return state.chatReducer;
   });
 
@@ -14,15 +19,26 @@ const ChatBox = (props) => {
       sx={{
         backgroundColor: 'white',
         borderRadius: '0.5rem',
-        display: { xs: chatState.selectedChat ? 'flex' : 'none', md: 'flex' },
+        display: { xs: selectedChat ? 'flex' : 'none', md: 'flex' },
         fontSize: '3rem',
         padding: '1rem',
         width: { xs: '100%', md: '74%' },
       }}
     >
-      <SingleChat fetchAgain={props.fetchAgain} setFetchAgain={props.setFetchAgain} />
+      {/* Must remember - lazy loading will not work without condition (as 'selectedChat === true' in our case) */}
+      {selectedChat && (
+        <Suspense fallback={<Spinner />}>
+          <SingleChat 
+            fetchAgain={fetchAgain} 
+            setFetchAgain={setFetchAgain} 
+          />
+        </Suspense>
+      )}
     </Box>
   );
 };
+
+// For debugging purposes.
+ChatBox.displayName = 'ChatBox';
 
 export default ChatBox;
